b4dd9b89a2ca348f1d981ba4bf54058f96837f3b
Henrique de Almeida Bento <72509185+Huntrall@users.noreply.github.com>
2025-03-21 11:23:41 -0300

    Update pyautoflinston.py

--- pyautoflinston.py
+++ pyautoflinston.py
@@ -2,7 +2,7 @@ import pyautogui as pyau
 import time
 import cv2
 import os
-"""
+
 pyau.press("win")
 time.sleep(1)
 
@@ -12,14 +12,14 @@ time.sleep(1)
 pyau.press("enter")
 time.sleep(2)
 
-email = pyau.locateOnScreen("email.png", confidence=0.8)
+email = pyau.locateOnScreen("images\\email.png", confidence=0.8)
 pyau.click(email)
 time.sleep(1)
 
 pyau.write("henrique.bento@delphostec.com.br")
 time.sleep(1)
 
-password = pyau.locateOnScreen("password.png", confidence=0.8)
+password = pyau.locateOnScreen("images\\password.png", confidence=0.8)
 pyau.click(password)
 time.sleep(1)
 
@@ -28,36 +28,36 @@ pyau.press("enter")
 time.sleep(3)
 
 try:
-    buttoncontinue = pyau.locateOnScreen("continue.png", confidence=0.8)
+    buttoncontinue = pyau.locateOnScreen("images\\continue.png", confidence=0.8)
     if buttoncontinue:
         pyau.click(buttoncontinue)
         time.sleep(70)
-        abrircomputador = pyau.locateOnScreen("abrircomputador.png", confidence=0.8)
+        abrircomputador = pyau.locateOnScreen("images\\abrircomputador.png", confidence=0.8)
         pyau.click(abrircomputador)
 except pyau.ImageNotFoundException:
-    print("Imagem 'continue.png' não encontrada, seguindo para o próximo passo...")
+    print("Imagem 'images\\continue.png' não encontrada, seguindo para o próximo passo...")
 
 time.sleep(2)
 
 try:
-    abrircomputador = pyau.locateOnScreen("abrircomputador.png", confidence=0.8)
+    abrircomputador = pyau.locateOnScreen("images\\abrircomputador.png", confidence=0.8)
     pyau.click(abrircomputador)
 except pyau.ImageNotFoundException:
-    print("Erro: 'abrircomputador.png' não encontrada.")
+    print("Erro: 'images\\brircomputador.png' não encontrada.")
 
 time.sleep(25)
 
-abrirsarwin = pyau.locateOnScreen("sarwin.png", confidence=0.8)
+abrirsarwin = pyau.locateOnScreen("images\\arwin.png", confidence=0.8)
 pyau.click(abrirsarwin, clicks=2)
 
 time.sleep(2)
 
-abriremp = pyau.locateOnScreen("emp.png", confidence=0.8)
+abriremp = pyau.locateOnScreen("images\\emp.png", confidence=0.8)
 pyau.click(abriremp, clicks=2)
 
 time.sleep(2)
 
-abrirdespesa = pyau.locateOnScreen("despesa.png", confidence=0.8)
+abrirdespesa = pyau.locateOnScreen("images\\despesa.png", confidence=0.8)
 pyau.click(abrirdespesa, clicks=2)
 
 time.sleep(4)
@@ -69,25 +69,25 @@ pyau.press("enter")
 
 time.sleep(7)
 
-abrirprocesso = pyau.locateOnScreen("processo.png", confidence=0.8)
+abrirprocesso = pyau.locateOnScreen("images\\processo.png", confidence=0.8)
 pyau.click(abrirprocesso, clicks=2)
 
 time.sleep(1)
 
-importadespesa = pyau.locateOnScreen("importadespesa.png", confidence=0.8)
+importadespesa = pyau.locateOnScreen("images\\importadespesa.png", confidence=0.8)
 pyau.click(importadespesa)
 
 time.sleep(1)
 
-clicaplanilha = pyau.locateOnScreen("planilha.png", confidence=0.8)
+clicaplanilha = pyau.locateOnScreen("images\\planilha.png", confidence=0.8)
 pyau.click(clicaplanilha)
 
-clicaabrirarquivo = pyau.locateOnScreen("abrirarquivo.png", confidence=0.8)
+clicaabrirarquivo = pyau.locateOnScreen("images\\abrirarquivo.png", confidence=0.8)
 pyau.click(clicaabrirarquivo)
 
 time.sleep(2)
 
-clicaestecomputador = pyau.locateOnScreen("estecomputador.png", confidence=0.8)
+clicaestecomputador = pyau.locateOnScreen("images\\estecomputador.png", confidence=0.8)
 pyau.click(clicaestecomputador)
 
 time.sleep(1)
@@ -103,20 +103,18 @@ pyau.scroll(-500)
 
 time.sleep(2)
 
-clicasetores = pyau.locateOnScreen("setores.png", confidence=0.8)
+clicasetores = pyau.locateOnScreen("images\\setores.png", confidence=0.8)
 pyau.click(clicasetores, clicks=2)
 
 time.sleep(0.5)
 
-clicati = pyau.locateOnScreen("TI.png", confidence=0.8)
+clicati = pyau.locateOnScreen("images\\TI.png", confidence=0.8)
 pyau.click(clicati, clicks=2)
 
 time.sleep(0.5)
 
-clicaautoop = pyau.locateOnScreen("autoop.png", confidence=0.8)
+clicaautoop = pyau.locateOnScreen("images\\autoop.png", confidence=0.8)
 pyau.click(clicaautoop, clicks=2)
-
-"""
 
 pasta = r"\\192.168.100.245\\Setores\\TI\\automacaoop"
 
